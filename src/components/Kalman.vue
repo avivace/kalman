8920a64c95580ff386d4d9a02e2514cca61722b1
Antonio Vivace <avivace4@gmail.com>
2019-02-02 00:27:28 +0100

    Comment things, mouse over the canvas or not

--- src/components/Kalman.vue
+++ src/components/Kalman.vue
@@ -1,33 +1,44 @@
 <template>
 	<div >
 	<h2>kalman</h2>
-	<canvas @pointermove="mouseOver" ref="ccont" width="600" height="600"></canvas>
+	<canvas @mouseleave="mouseLeave" @pointermove="mouseOver" ref="ccont" :width="width" :height="height"></canvas>
 	<br><br><br>
 	{{ Number((fps).toFixed(0)) }} FPS
+  {{ status }}
 	</div>
 </template>
 
 <script>
 export default {
 	data: () =>  ({
-		a: null,
+    // Canvas size
+    width: 600,
+    height: 600,
 		ctx: null,
+    // (Expected) framerate
+    framerate: 60,
+    // Performance
 		lastCalledTime: null,
-		fps: 0,
+		fps: 0, // (Measured) framerate
 		framecount: 0,
+    status: "paused"
 	}),
 	mounted () {
 
 		this.init();
 		this.frame();
 
-
 	},
 	methods: {
 		getRandomInt(max) {
 			return Math.floor(Math.random() * Math.floor(max));
 		},
+    mouseLeave(){
+      console.log("Mouse left, stopping")
+      this.status = "paused"
+    },
 		mouseOver(event){
+      this.status = "running"
 			let canvas = this.$refs.ccont;
 			var rect = canvas.getBoundingClientRect();
 
@@ -37,13 +48,11 @@ export default {
 		init() {
 			var c = this.$refs.ccont;
 			this.ctx = c.getContext("2d");
+      this.ctx.fillStyle = 'blue';
+      this.ctx.fillRect(0, 0, 600, 600);
 		},
 		frame() {
-			this.framecount++;
 			let ctx = this.ctx
-			//ctx.fillStyle = 'blue';
-			//ctx.fillRect(0, 0, 600, 600);
-			
 
 			ctx.fillStyle = 'red';
 			ctx.fillRect(this.clientX, this.clientY,50,50);
@@ -58,12 +67,15 @@ export default {
 			this.lastCalledTime = performance.now();
 
 			// Update the fps counter every 5 rendered frames
-			// i.e. every 1000/fps * 5 milliseconds 
-			if (this.framecount % 5 == 0)
+			// i.e. every 1000/expectedFrameRate * 5 milliseconds 
+			if (this.framecount % 5 == 0){
 				this.fps = 1/delta;
-
-			// Desired framerate
-			setTimeout(this.frame, 1000/1000);
+        this.framecount = 0;
+      }
+      // We done
+      this.framecount++;
+			// See ya in 1000/desiredFramerate milliseconds
+			setTimeout(this.frame, 1000/this.framerate);
 
 		}
 	}
