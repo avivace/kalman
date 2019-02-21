9f9bf30bc6669e29f562b0ae2d8516a76516e17e
Antonio Vivace <avivace4@gmail.com>
2019-02-21 10:32:31 +0100

    format

--- src/components/Kalman.vue
+++ src/components/Kalman.vue
@@ -1,34 +1,31 @@
 <template>
-	<div >
-    <!--
-	<h2 class="title">Kalman Filter demo</h2>
-  <h3 class="subtitle">Antonio Vivace, May 2019</h3>
--->
-	<canvas @mouseleave="mouseLeave" @pointermove="mouseOver" ref="ccont" :width="width" :height="height"></canvas>
-	<br><br><br>
-  <span class="stats">
-	{{ Number((fps).toFixed(0)) }} FPS
-  {{ status }}
-  {{ states.length }}
-</span>
+	<div>
+		<h2 class="title">Kalman Filter demo</h2>
+		<h3 class="subtitle">Antonio Vivace, May 2019</h3>
+		<canvas @mouseleave="mouseLeave" @pointermove="mouseOver" ref="ccont" :width="width" :height="height"></canvas>
+		<br><br><br>
+		<span class="stats">
+			{{ Number((fps).toFixed(0)) }} FPS
+			{{ status }}
+			{{ states.length }}
+		</span>
 	</div>
 </template>
-
 <script>
 export default {
-	data: () =>  ({
-    // Canvas size
-    width: 600,
-    height: 600,
+	data: () => ({
+		// Canvas size
+		width: 600,
+		height: 600,
 		ctx: null,
-    // Performance
+		// Performance
 		lastCalledTime: null,
 		fps: 0, // (Measured) framerate
 		framecount: 0,
-    status: "paused",
-    states: []
+		status: "paused",
+		states: []
 	}),
-	mounted () {
+	mounted() {
 
 		this.init();
 		this.frame();
@@ -38,12 +35,12 @@ export default {
 		getRandomInt(max) {
 			return Math.floor(Math.random() * Math.floor(max));
 		},
-    mouseLeave(){
-      console.log("Mouse left, stopping")
-      this.status = "paused"
-    },
-		mouseOver(event){
-      this.status = "running"
+		mouseLeave() {
+			console.log("Mouse left, stopping")
+			this.status = "paused"
+		},
+		mouseOver(event) {
+			this.status = "running"
 			let canvas = this.$refs.ccont;
 			var rect = canvas.getBoundingClientRect();
 
@@ -51,148 +48,145 @@ export default {
 			this.clientY = event.clientY - rect.top;
 		},
 		init() {
-  var A = window.$M([
-    [1, 0, 0.2, 0],
-    [0, 1, 0, 0.2],
-    [0, 0, 1, 0],
-    [0, 0, 0, 1]
-  ]);
-  
-  var B = window.$M([
-    [1, 0, 0, 0],
-    [0, 1, 0, 0],
-    [0, 0, 1, 0],
-    [0, 0, 0, 1]
-  ]);
+			var A = window.$M([
+				[1, 0, 0.2, 0],
+				[0, 1, 0, 0.2],
+				[0, 0, 1, 0],
+				[0, 0, 0, 1]
+			]);
 
-  var H = window.$M([
-    [1, 0, 0, 0],
-    [0, 1, 0, 0],
-    [0, 0, 1, 0],
-    [0, 0, 0, 1]
-  ]);
+			var B = window.$M([
+				[1, 0, 0, 0],
+				[0, 1, 0, 0],
+				[0, 0, 1, 0],
+				[0, 0, 0, 1]
+			]);
 
-  var Q = window.$M([
-    [0.001, 0, 0, 0],
-    [0, 0.001, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0]
-  ]);
+			var H = window.$M([
+				[1, 0, 0, 0],
+				[0, 1, 0, 0],
+				[0, 0, 1, 0],
+				[0, 0, 0, 1]
+			]);
 
-  var R = window.$M([
-    [0.1, 0, 0, 0],
-    [0, 0.1, 0, 0],
-    [0, 0, 0.1, 0],
-    [0, 0, 0, 0.1]
-  ]);
+			var Q = window.$M([
+				[0.001, 0, 0, 0],
+				[0, 0.001, 0, 0],
+				[0, 0, 0, 0],
+				[0, 0, 0, 0]
+			]);
 
-  var last_x = window.$V([0, 0, 0, 0]);
-  
-  var last_P = window.$M([
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0]
-  ]);
+			var R = window.$M([
+				[0.1, 0, 0, 0],
+				[0, 0.1, 0, 0],
+				[0, 0, 0.1, 0],
+				[0, 0, 0, 0.1]
+			]);
 
-        console.log(A)
+			var last_x = window.$V([0, 0, 0, 0]);
+
+			var last_P = window.$M([
+				[0, 0, 0, 0],
+				[0, 0, 0, 0],
+				[0, 0, 0, 0],
+				[0, 0, 0, 0]
+			]);
+
+			console.log(A)
 			var c = this.$refs.ccont;
-			this.ctx = c.getContext("2d",{ alpha: false });
-      this.ctx.fillStyle = "#37474f";
-      this.ctx.fillRect(0, 0, 600, 600);
+			this.ctx = c.getContext("2d", { alpha: false });
+			this.ctx.fillStyle = "#37474f";
+			this.ctx.fillRect(0, 0, 600, 600);
 
-      this.State = class State {
-        constructor(realInput, noisyInput) {
-          this.realInput = realInput;
-          this.noisyInput = noisyInput;
-          this.predictedPoint = null;
-          this.dead = false;
-        }
+			this.State = class State {
+				constructor(realInput, noisyInput) {
+					this.realInput = realInput;
+					this.noisyInput = noisyInput;
+					this.predictedPoint = null;
+					this.dead = false;
+				}
 
-        display() {
-          this.realInput.display("red")
-          this.noisyInput.display("#558b2f")
-        }
+				display() {
+					this.realInput.display("red")
+					this.noisyInput.display("#558b2f")
+				}
 
-        update() {
-          this.realInput.update()
-          this.noisyInput.update()
-          if (this.realInput.ttl == 0)
-            this.dead = true
-        }
+				update() {
+					this.realInput.update()
+					this.noisyInput.update()
+					if (this.realInput.ttl == 0)
+						this.dead = true
+				}
 
-      }
+			}
 
-      this.Point = class Point {
-        constructor(x, y, ctx){
-          this.x = x;
-          this.y = y;
-          this.ttl = 50;
-          this.ctx = ctx;
-        }
+			this.Point = class Point {
+				constructor(x, y, ctx) {
+					this.x = x;
+					this.y = y;
+					this.ttl = 520;
+					this.ctx = ctx;
+				}
 
-        display(color) {
-          this.ctx.fillStyle = color;
-          this.ctx.fillRect(this.x, this.y, 5, 5);
-          
-          
-          
-        }
+				display(color) {
+					this.ctx.fillStyle = color;
+					this.ctx.fillRect(this.x, this.y, 5, 5);
 
-        update() {
-          this.ttl--;
-        }
-      }
+
+
+				}
+
+				update() {
+					this.ttl--;
+				}
+			}
 
 		},
 		frame() {
 
 			let ctx = this.ctx
-      this.ctx.fillStyle = "#37474f";
-      this.ctx.fillRect(0, 0, 600, 600);
+			this.ctx.fillStyle = "#37474f";
+			this.ctx.fillRect(0, 0, 600, 600);
 
-      let a = new this.Point(this.clientX - 3, this.clientY - 3, ctx)
-      let noiseX = (Math.random() * (50 + 1) ) << 0
-      let noiseY = (Math.random() * (50 + 1) ) << 0
-      let noisyA = new this.Point(this.clientX + noiseX -25, this.clientY + noiseY-25, ctx)
-      this.states.push(new this.State(a, noisyA))
+			let a = new this.Point(this.clientX - 3, this.clientY - 3, ctx)
+			let noiseX = (Math.random() * (50 + 1)) << 0
+			let noiseY = (Math.random() * (50 + 1)) << 0
+			let noisyA = new this.Point(this.clientX + noiseX - 25, this.clientY + noiseY - 25, ctx)
+			this.states.push(new this.State(a, noisyA))
 
-      this.states.forEach(function(state, i){
-        //console.log(i)
-        state.display();
-        state.update();
-        if (state.dead){
-            delete this.state
-            this.states.splice(i, 1)
-        }
-      }.bind(this))
+			this.states.forEach(function(state, i) {
+				//console.log(i)
+				state.display();
+				state.update();
+				if (state.dead) {
+					delete this.state
+					this.states.splice(i, 1)
+				}
+			}.bind(this))
 
-      requestAnimationFrame(this.frame)
+			requestAnimationFrame(this.frame)
 
 		}
 	}
 }
-
-
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
-.stats{
-  font-size:1.5rem;
+.stats {
+	font-size: 1.5rem;
 }
 
 .title {
-  font-weight: 700;
-  font-size: 4rem;
-  letter-spacing: -0.07em;
-  line-height: 0;
+	font-weight: 700;
+	font-size: 4rem;
+	letter-spacing: -0.07em;
+	line-height: 0;
 }
+
 .subtitle {
-  font-weight: 500;
-  font-size: 1.7rem;
-  letter-spacing: -0.07em;
-  line-height: 0;
+	font-weight: 500;
+	font-size: 1.7rem;
+	letter-spacing: -0.07em;
+	line-height: 0;
 }
 </style>
--- src/components/p5.vue
+++ src/components/p5.vue
@@ -1,127 +1,128 @@
 <template>
-  <div>
-    Using p5.js with Vue binding
-    <br>
-    <input v-model="x" placeholder="x">
-    <input v-model="y" placeholder="y">
-    <br><br>
-    <div ref="canvas"></div>
-    {{ states.length }}
-    {{ Math.round(framerate) }}
-  </div>
+	<div>
+		Using p5.js with Vue binding
+		<br>
+		<input v-model="x" placeholder="x">
+		<input v-model="y" placeholder="y">
+		<br><br>
+		<div ref="canvas"></div>
+		{{ states.length }}
+		{{ Math.round(framerate) }}
+	</div>
 </template>
-
 <script>
 export default {
-  data: function () {
-    return {
-      script: null,
-      ps: null,
-      x: 200,
-      y: 200,
-      a: 0,
-      canvas: null,
-      states: [],
-      framerate: 0,
-    }
-  },
-  mounted () {
-    this.script = (p) => {
-      //this.states = []
-      /*
-      A state is a list of points:
-      RealInput: Real registered mouse position
-      NoisyInput: Set of randomised noisy input around RealInput
-      PredictedPoint: Output of the filter
-      */
-      class State {
-        constructor() {
-          this.realInput = new Point(p.mouseX, p.mouseY);
-          this.noisyInput = []
-          this.predictedPoint = null;
-          this.dead = false;
-        }
+	data: function() {
+		return {
+			script: null,
+			ps: null,
+			x: 200,
+			y: 200,
+			a: 0,
+			canvas: null,
+			states: [],
+			framerate: 0,
+		}
+	},
+	mounted() {
+		this.script = (p) => {
+			//this.states = []
+			/*
+			A state is a list of points:
+			RealInput: Real registered mouse position
+			NoisyInput: Set of randomised noisy input around RealInput
+			PredictedPoint: Output of the filter
+			*/
+			class State {
+				constructor() {
+					this.realInput = new Point(p.mouseX, p.mouseY);
+					this.noisyInput = []
+					this.predictedPoint = null;
+					this.dead = false;
+				}
 
-      display() {
-        this.realInput.display()
-      }
+				display() {
+					this.realInput.display()
+				}
 
-      update() {
-        this.realInput.update()
-        if (this.realInput.ttl == 0)
-          this.dead = true
-      }
+				update() {
+					this.realInput.update()
+					if (this.realInput.ttl == 0)
+						this.dead = true
+				}
 
-      }
+			}
 
-      class Point {
-        constructor(x, y){
-          this.x = x;
-          this.y = y;
-          this.ttl = 60;
-        }
+			class Point {
+				constructor(x, y) {
+					this.x = x;
+					this.y = y;
+					this.ttl = 60;
+				}
 
-        display() {
-          p.fill(this.ttl + 180)
-          p.ellipse(this.x, this.y, 8, 8)
-        }
+				display() {
+					p.fill(this.ttl + 180)
+					p.ellipse(this.x, this.y, 8, 8)
+				}
 
-        update() {
-          this.ttl--;
-        }
-      }
+				update() {
+					this.ttl--;
+				}
+			}
 
 
 
-      p.setup = () => {
-        this.canvas = p.createCanvas(400, 400)
-        this.canvas.parent(this.$refs.canvas)
-        p.frameRate(60)
-      }
+			p.setup = () => {
+				this.canvas = p.createCanvas(400, 400)
+				this.canvas.parent(this.$refs.canvas)
+				p.frameRate(60)
+			}
 
-      p.draw = () => {
-        
-        p.background(0)
-        this.states.push(new State())
+			p.draw = () => {
 
-        p.fill(255)
-        p.ellipse(p.mouseX, p.mouseY, 8, 8)
-        
-        
-        for( let i = 0; i < this.states.length; i++) {
-          this.states[i].update();
-          this.states[i].display();
-          if (this.states[i].dead){
-            delete this.states[i]
-            this.states.splice(i, 1)
-          }
-        }
-        
-        this.framerate = p.frameRate()
-        
-      }
-    }
+				p.background(0)
+				this.states.push(new State())
 
-    const P5 = require('p5')
-    this.ps = new P5(this.script)
-  }
+				p.fill(255)
+				p.ellipse(p.mouseX, p.mouseY, 8, 8)
+
+
+				for (let i = 0; i < this.states.length; i++) {
+					this.states[i].update();
+					this.states[i].display();
+					if (this.states[i].dead) {
+						delete this.states[i]
+						this.states.splice(i, 1)
+					}
+				}
+
+				this.framerate = p.frameRate()
+
+			}
+		}
+
+		const P5 = require('p5')
+		this.ps = new P5(this.script)
+	}
 }
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
-  margin: 40px 0 0;
+	margin: 40px 0 0;
 }
+
 ul {
-  list-style-type: none;
-  padding: 0;
+	list-style-type: none;
+	padding: 0;
 }
+
 li {
-  display: inline-block;
-  margin: 0 10px;
+	display: inline-block;
+	margin: 0 10px;
 }
+
 a {
-  color: #42b983;
+	color: #42b983;
 }
 </style>
