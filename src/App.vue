75ceda478a44f40b327e04fa734d71794f04f65e
Antonio Vivace <avivace4@gmail.com>
2019-02-21 08:40:07 +0100

    try things with requestAnimFrame and Sylvester

--- src/App.vue
+++ src/App.vue
@@ -16,12 +16,18 @@ export default {
 </script>
 
 <style>
+@import url('https://rsms.me/inter/inter.css');
+html { font-family: 'Inter', sans-serif; }
+@supports (font-variation-settings: normal) {
+  html { font-family: 'Inter var', sans-serif; }
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Inter','Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
--- src/components/Kalman.vue
+++ src/components/Kalman.vue
@@ -1,11 +1,16 @@
 <template>
 	<div >
-	<h2>kalman</h2>
+    <!--
+	<h2 class="title">Kalman Filter demo</h2>
+  <h3 class="subtitle">Antonio Vivace, May 2019</h3>
+-->
 	<canvas @mouseleave="mouseLeave" @pointermove="mouseOver" ref="ccont" :width="width" :height="height"></canvas>
 	<br><br><br>
+  <span class="stats">
 	{{ Number((fps).toFixed(0)) }} FPS
   {{ status }}
   {{ states.length }}
+</span>
 	</div>
 </template>
 
@@ -16,8 +21,6 @@ export default {
     width: 600,
     height: 600,
 		ctx: null,
-    // (Expected) framerate
-    framerate: 60,
     // Performance
 		lastCalledTime: null,
 		fps: 0, // (Measured) framerate
@@ -48,25 +51,72 @@ export default {
 			this.clientY = event.clientY - rect.top;
 		},
 		init() {
+  var A = window.$M([
+    [1, 0, 0.2, 0],
+    [0, 1, 0, 0.2],
+    [0, 0, 1, 0],
+    [0, 0, 0, 1]
+  ]);
+  
+  var B = window.$M([
+    [1, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 1, 0],
+    [0, 0, 0, 1]
+  ]);
+
+  var H = window.$M([
+    [1, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 1, 0],
+    [0, 0, 0, 1]
+  ]);
+
+  var Q = window.$M([
+    [0.001, 0, 0, 0],
+    [0, 0.001, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+  ]);
+
+  var R = window.$M([
+    [0.1, 0, 0, 0],
+    [0, 0.1, 0, 0],
+    [0, 0, 0.1, 0],
+    [0, 0, 0, 0.1]
+  ]);
+
+  var last_x = window.$V([0, 0, 0, 0]);
+  
+  var last_P = window.$M([
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+  ]);
+
+        console.log(A)
 			var c = this.$refs.ccont;
-			this.ctx = c.getContext("2d");
-      this.ctx.fillStyle = 'blue';
+			this.ctx = c.getContext("2d",{ alpha: false });
+      this.ctx.fillStyle = "#37474f";
       this.ctx.fillRect(0, 0, 600, 600);
 
       this.State = class State {
-        constructor(realInput) {
+        constructor(realInput, noisyInput) {
           this.realInput = realInput;
-          this.noisyInput = []
+          this.noisyInput = noisyInput;
           this.predictedPoint = null;
           this.dead = false;
         }
 
         display() {
-          this.realInput.display()
+          this.realInput.display("red")
+          this.noisyInput.display("#558b2f")
         }
 
         update() {
           this.realInput.update()
+          this.noisyInput.update()
           if (this.realInput.ttl == 0)
             this.dead = true
         }
@@ -77,13 +127,13 @@ export default {
         constructor(x, y, ctx){
           this.x = x;
           this.y = y;
-          this.ttl = 600;
+          this.ttl = 50;
           this.ctx = ctx;
         }
 
-        display() {
-          this.ctx.fillStyle = 'red';
-          this.ctx.fillRect(this.x, this.y, 6, 6);
+        display(color) {
+          this.ctx.fillStyle = color;
+          this.ctx.fillRect(this.x, this.y, 5, 5);
           
           
           
@@ -96,12 +146,16 @@ export default {
 
 		},
 		frame() {
+
 			let ctx = this.ctx
-      this.ctx.fillStyle = 'blue';
+      this.ctx.fillStyle = "#37474f";
       this.ctx.fillRect(0, 0, 600, 600);
 
       let a = new this.Point(this.clientX - 3, this.clientY - 3, ctx)
-      this.states.push(new this.State(a))
+      let noiseX = (Math.random() * (50 + 1) ) << 0
+      let noiseY = (Math.random() * (50 + 1) ) << 0
+      let noisyA = new this.Point(this.clientX + noiseX -25, this.clientY + noiseY-25, ctx)
+      this.states.push(new this.State(a, noisyA))
 
       this.states.forEach(function(state, i){
         //console.log(i)
@@ -112,22 +166,8 @@ export default {
             this.states.splice(i, 1)
         }
       }.bind(this))
-			
 
-			// Compute the real framerate
-			let delta = (performance.now() - this.lastCalledTime)/1000;
-			this.lastCalledTime = performance.now();
-
-			// Update the fps counter every 5 rendered frames
-			// i.e. every 1000/expectedFrameRate * 5 milliseconds 
-			if (this.framecount % 5 == 0){
-				this.fps = 1/delta;
-        this.framecount = 0;
-      }
-      // We done
-      this.framecount++;
-			// See ya in 1000/desiredFramerate milliseconds
-			setTimeout(this.frame, 1000/this.framerate);
+      requestAnimationFrame(this.frame)
 
 		}
 	}
@@ -139,4 +179,20 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
+.stats{
+  font-size:1.5rem;
+}
+
+.title {
+  font-weight: 700;
+  font-size: 4rem;
+  letter-spacing: -0.07em;
+  line-height: 0;
+}
+.subtitle {
+  font-weight: 500;
+  font-size: 1.7rem;
+  letter-spacing: -0.07em;
+  line-height: 0;
+}
 </style>
