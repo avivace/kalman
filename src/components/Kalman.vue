269bf2243cfd555c791f1c5080223fcb5fa102f6
Antonio Vivace <avivace4@gmail.com>
2019-06-25 19:51:58 +0200

    Gaussian implementation

--- src/components/Kalman.vue
+++ src/components/Kalman.vue
@@ -86,11 +86,76 @@
 	</div>
 </template>
 <script>
+// randomGaussian taken from http://www.ollysco.de/2012/04/gaussian-normal-functions-in-javascript.html
+(function() {
+	/**
+	 * Returns a Gaussian Random Number around a normal distribution defined by the mean
+	 * and standard deviation parameters.
+	 *
+	 * Uses the algorithm used in Java's random class, which in turn comes from
+	 * Donald Knuth's implementation of the BoxÐMuller transform.
+	 *
+	 * @param {Number} [mean = 0.0] The mean value, default 0.0
+	 * @param {Number} [standardDeviation = 1.0] The standard deviation, default 1.0
+	 * @return {Number} A random number
+	 */
+	Math.randomGaussian = function(mean, standardDeviation) {
+		mean = defaultTo(mean, 0.0);
+		standardDeviation = defaultTo(standardDeviation, 1.0);
+
+		if (Math.randomGaussian.nextGaussian !== undefined) {
+			var nextGaussian = Math.randomGaussian.nextGaussian;
+			delete Math.randomGaussian.nextGaussian;
+			return nextGaussian * standardDeviation + mean;
+		} else {
+			var v1, v2, s, multiplier;
+			do {
+				v1 = 2 * Math.random() - 1; // between -1 and 1
+				v2 = 2 * Math.random() - 1; // between -1 and 1
+				s = v1 * v1 + v2 * v2;
+			} while (s >= 1 || s == 0);
+			multiplier = Math.sqrt((-2 * Math.log(s)) / s);
+			Math.randomGaussian.nextGaussian = v2 * multiplier;
+			return v1 * multiplier * standardDeviation + mean;
+		}
+	};
+
+	/**
+	 * Returns a normal probability density function for the given parameters.
+	 * The function will return the probability for given values of X
+	 *
+	 * @param {Number} [mean = 0] The center of the peak, usually at X = 0
+	 * @param {Number} [standardDeviation = 1.0] The width / standard deviation of the peak
+	 * @param {Number} [maxHeight = 1.0] The maximum height of the peak, usually 1
+	 * @returns {Function} A function that will return the value of the distribution at given values of X
+	 */
+	Math.getGaussianFunction = function(mean, standardDeviation, maxHeight) {
+		mean = defaultTo(mean, 0.0);
+		standardDeviation = defaultTo(standardDeviation, 1.0);
+		maxHeight = defaultTo(maxHeight, 1.0);
+
+		return function getNormal(x) {
+			return (
+				maxHeight *
+				Math.pow(
+					Math.E,
+					-Math.pow(x - mean, 2) /
+						(2 * (standardDeviation * standardDeviation))
+				)
+			);
+		};
+	};
+
+	function defaultTo(value, defaultValue) {
+		return isNaN(value) ? defaultValue : value;
+	}
+})();
+
 export default {
 	data: () => ({
 		// Canvas size
-		width: 600,
-		height: 600,
+		width: 800,
+		height: 800,
 		ctx: null,
 		// Performance
 		lastCalledTime: null,
