c2049b68cedc60865d32b00d3b09be72a9f75620
Antonio Vivace <avivace4@gmail.com>
2019-06-25 08:57:56 +0200

    Format

--- src/components/Kalman.vue
+++ src/components/Kalman.vue
@@ -2,22 +2,41 @@
 	<div>
 		<h2 class="title">Kalman Filter 2D demo</h2>
 		<h3 class="subtitle">Antonio Vivace, May 2019</h3>
-		
-		Width
-		<input type="range" min="0" :max="1800" value="75" class="slider" v-model="width">
-		{{ width }}px
-		Height
-		<input type="range" min="0" :max="1000" value="75" class="slider" v-model="height">
-		{{ height }}px
 
+		Width
+		<input
+			type="range"
+			min="0"
+			:max="1800"
+			value="75"
+			class="slider"
+			v-model="width"
+		/>
+		{{ width }}px Height
+		<input
+			type="range"
+			min="0"
+			:max="1000"
+			value="75"
+			class="slider"
+			v-model="height"
+		/>
+		{{ height }}px
 
 		<mu-container>
 			Max Noise: {{ maxNoise }}
-			<mu-slider :min="0" :max="width/3" :step=1 class="demo-slider" v-model="maxNoise"></mu-slider>
+			<mu-slider
+				:min="0"
+				:max="width / 3"
+				:step="1"
+				class="demo-slider"
+				v-model="maxNoise"
+			></mu-slider>
 		</mu-container>
 
-		<br><br>
-		<canvas class="kalmandemo"
+		<br /><br />
+		<canvas
+			class="kalmandemo"
 			@mouseleave="mouseLeave"
 			@pointermove="mouseOver"
 			ref="ccont"
@@ -25,13 +44,20 @@
 			:height="height"
 		></canvas>
 		<br /><br /><br />
-		
+
 		<span class="stats">
-			{{ status }}, <code>{{ states.length }}</code> states drawn. <code>{{ Math.round(ms) }}</code> ms per frame
+			{{ status }}, <code>{{ states.length }}</code> states drawn.
+			<code>{{ Math.round(ms) }}</code> ms per frame
 		</span>
-	<small >
-	<p><b><a href="https://github.com/avivace/kalman">Source code</a></b></p>
-	</small>
+		<small>
+			<p>
+				<b
+					><a href="https://github.com/avivace/kalman"
+						>Source code</a
+					></b
+				>
+			</p>
+		</small>
 	</div>
 </template>
 <script>
@@ -56,7 +82,7 @@ export default {
 		R: null,
 		last: null,
 		maxNoise: 75,
-		ms: 0,
+		ms: 0
 	}),
 	mounted() {
 		this.init();
@@ -78,7 +104,7 @@ export default {
 			this.clientY = event.clientY - rect.top;
 		},
 		init() {
-			// Sylvester is available under the window context, since we imported 
+			// Sylvester is available under the window context, since we imported
 			//  it globally in the html template.
 			let m = window.$M;
 			let v = window.$V;
@@ -114,7 +140,7 @@ export default {
 				[0, 0, 0, 0.1]
 			]);
 
-			this.lastPoint = v([0, 0, 0, 0])
+			this.lastPoint = v([0, 0, 0, 0]);
 
 			this.last = m([
 				[0, 0, 0, 0],
@@ -163,9 +189,9 @@ export default {
 					// Map remaining TTL to 0-255, convert it to two hex digits
 					//  and use it as Alpha channel (ttl -> 0, alpha -> 1)
 
-					let alpha = Math.round(this.ttl * 255/200).toString(16)
+					let alpha = Math.round((this.ttl * 255) / 200).toString(16);
 					this.ctx.beginPath();
-					this.ctx.arc(this.x, this.y, 2, 0, 2*Math.PI)
+					this.ctx.arc(this.x, this.y, 2, 0, 2 * Math.PI);
 					this.ctx.fillStyle = color + alpha;
 					this.ctx.fill();
 					//this.ctx.fillRect(this.x, this.y, 4, 4);
@@ -178,26 +204,26 @@ export default {
 		},
 		frame() {
 			let start = performance.now();
-			if (this.status == "running"){
+			if (this.status == "running") {
 				let v = window.$V;
-				
-				
 
 				let ctx = this.ctx;
 				this.ctx.fillStyle = "#263238";
 				this.ctx.fillRect(0, 0, this.width, this.height);
 
-				let maxNoise = this.maxNoise
+				let maxNoise = this.maxNoise;
 
 				// Real point
 				let a = new this.Point(this.clientX - 3, this.clientY - 3, ctx);
 				// Add noise to the clean input
-				let noisyX = Math.round(this.clientX + this.getRandomInt(maxNoise) - maxNoise/2)
-				let noisyY = Math.round(this.clientY + this.getRandomInt(maxNoise) - maxNoise/2)
+				let noisyX = Math.round(
+					this.clientX + this.getRandomInt(maxNoise) - maxNoise / 2
+				);
+				let noisyY = Math.round(
+					this.clientY + this.getRandomInt(maxNoise) - maxNoise / 2
+				);
 
-				let n = new this.Point(noisyX, noisyY, ctx)
-
-
+				let n = new this.Point(noisyX, noisyY, ctx);
 
 				/* 
 
@@ -211,19 +237,22 @@ export default {
 				let deltaX = noisyX - this.lastPoint.elements[0];
 				let deltaY = noisyY - this.lastPoint.elements[1];
 				let measurement = v([noisyX, noisyY, deltaX, deltaY]);
-				
 
 				// PREDICTION step
 				// x = (A * x) + (B * c)
-				var x = this.A.multiply(this.lastPoint)
+				var x = this.A.multiply(this.lastPoint);
 				// P = (A * P * AT) + Q
-				var P = ((this.A.multiply(this.last)).multiply(this.A.transpose())).add(this.Q); 
+				var P = this.A.multiply(this.last)
+					.multiply(this.A.transpose())
+					.add(this.Q);
 
 				// CORRECTION step
-				// S = (H * P * HT) + R 
-				var S = ((this.H.multiply(P)).multiply(this.H.transpose())).add(this.R);
+				// S = (H * P * HT) + R
+				var S = this.H.multiply(P)
+					.multiply(this.H.transpose())
+					.add(this.R);
 				// K = P * HT * S-1
-				var K = (P.multiply(this.H.transpose())).multiply(S.inverse());
+				var K = P.multiply(this.H.transpose()).multiply(S.inverse());
 				// y = m - (H * x)
 				var y = measurement.subtract(this.H.multiply(x));
 
@@ -231,11 +260,17 @@ export default {
 				//  this is the final filtered point for this iteration
 				this.lastPoint = x.add(K.multiply(y));
 				// P = (I - (K * H)) * P
-				this.last = ((window.Matrix.I(4)).subtract(K.multiply(this.H))).multiply(P);
+				this.last = window.Matrix.I(4)
+					.subtract(K.multiply(this.H))
+					.multiply(P);
 
 				// ---
 
-				let k = new this.Point(this.lastPoint.elements[0], this.lastPoint.elements[1], ctx)
+				let k = new this.Point(
+					this.lastPoint.elements[0],
+					this.lastPoint.elements[1],
+					ctx
+				);
 
 				// Push the final state (real, noisy, filtered)
 				this.states.push(new this.State(a, n, k));
@@ -253,7 +288,7 @@ export default {
 			// See ya in 1000/desiredFramerate milliseconds
 			this.lastCalledTime = performance.now();
 			let ms = performance.now() - start;
-			this.ms = ms
+			this.ms = ms;
 			setTimeout(this.frame, 1000 / this.framerate - ms);
 		}
 	}
@@ -283,7 +318,6 @@ a {
 }
 
 .kalmandemo {
-	box-shadow: 0 27.125px 50px -12.125px rgba(0,0,0,0.4);
+	box-shadow: 0 27.125px 50px -12.125px rgba(0, 0, 0, 0.4);
 }
-
 </style>
